62243850754c93de8cd74968deba9de36b71aba2
MarioGoDevLike <marionassar57@gmail.com>
2023-06-22 21:13:59 +0300

    add: song screen

--- lib/screens/song_screen.dart
+++ lib/screens/song_screen.dart
@@ -1,9 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import '../models/song_model.dart';
+import '../widgets/seekbar.dart';
+import 'package:rxdart/rxdart.dart' as rxdart;
 
-class SongScreen extends StatelessWidget {
+class SongScreen extends StatefulWidget {
   const SongScreen({super.key});
+
+  @override
+  State<SongScreen> createState() => _SongScreenState();
+}
+
+class _SongScreenState extends State<SongScreen> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  Song song = Song.songs[0];
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer.setAudioSource(ConcatenatingAudioSource(children: [
+      AudioSource.uri(
+        Uri.parse('asset:///${song.url}'),
+      ),
+    ]));
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Stream<SeekBarData> get _seekBarDataStream =>
+      rxdart.Rx.combineLatest2<Duration, Duration?, SeekBarData>(
+          audioPlayer.positionStream, audioPlayer.durationStream, (
+        Duration position,
+        Duration? duration,
+      ) {
+        return SeekBarData(
+          position,
+          duration ?? Duration.zero,
+        );
+      });
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            song.coverUrl,
+            fit: BoxFit.cover,
+          ),
+          const _BackgroundFilter(),
+          _MusicPlayer(
+              seekBarDataStream: _seekBarDataStream, audioPlayer: audioPlayer),
+        ],
+      ),
+    );
+  }
+}
+
+class _MusicPlayer extends StatelessWidget {
+  const _MusicPlayer({
+    super.key,
+    required Stream<SeekBarData> seekBarDataStream,
+    required this.audioPlayer,
+  }) : _seekBarDataStream = seekBarDataStream;
+
+  final Stream<SeekBarData> _seekBarDataStream;
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+        vertical: 40.0,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          StreamBuilder<SeekBarData>(
+              stream: _seekBarDataStream,
+              builder: (context, snapshot) {
+                final positionData = snapshot.data;
+                return SeekBar(
+                  position: positionData?.position ?? Duration.zero,
+                  duration: positionData?.duration ?? Duration.zero,
+                  onChangeEnd: audioPlayer.seek,
+                );
+              }),
+        ],
+      ),
+    );
+  }
+}
+
+class _BackgroundFilter extends StatelessWidget {
+  const _BackgroundFilter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (rect) {
+        return LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Colors.white.withOpacity(0.5),
+              Colors.white.withOpacity(0.0),
+            ]).createShader(rect);
+      },
+      blendMode: BlendMode.dstOut,
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade800,
+            ])),
+      ),
+    );
   }
 }
